2556ba42d0122a926de3d0c33004ff58910b6cb3
Adam <you@example.com>
2019-07-03 11:46:03 -0400

    added payment

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import stripe
+import download
 
 app = Flask(__name__)
 
+pub_key = "pk_test_ZTlu8ZeGhxcvr013B9tXDcji008aGb26fG"
+sec_key = "sk_test_NISIDuFJvjhYaRrSXF9Yn3gd006rKr2Xd8"
+
+stripe.api_key = sec_key
+
 @app.route('/')
 
-def index():
-    return render_template('index.html')
 
-@app.route('/pay')
+def index():
+    return render_template('home.html')
+
+@app.route('/getstarted')
 def form():
-    return render_template('payment.html')
+    return render_template('payment.html',pub_key=pub_key)
+
+@app.route('/pay',methods=["POST"])
+def pay():
+    try:
+        download.get_all_tweets(request.form.get('username', type=str))
+    except TypeError:
+        return render_template('form.html', message='invalid @')
+    customer = stripe.Customer.create(email=request.form['stripeEmail'], source=request.form['stripeToken'])
+    charge = stripe.Charge.create(customer=customer.id, amount=500, currency='usd',description='sentiment analysis')
+    return redirect(url_for('diagnostic'))
+
+@app.route('/diagnostic')
+def diagnostic():
+    return render_template('diagnostic.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ download.py
@@ -0,0 +1,55 @@
+import tweepy #https://github.com/tweepy/tweepy
+import csv
+
+#Twitter API credentials
+consumer_key = "cFUoFRx22fyI4ljTcLxySO27r"
+consumer_secret = "xl04xe4916p2FUIUeRefzNLgzKPyU1Cb8poRoHmUTex9UICAcM"
+access_key = "2586611642-x5Ld1txncvSpoD3HdoMDV4QTGC8Ll5aZY2Wwtlr"
+access_secret = "liimxz32b9CiMOPV1rQ6XLSvTJW1HV1p5B5TDxbR2qHPm"
+
+
+def get_all_tweets(screen_name):
+	#Twitter only allows access to a users most recent 3240 tweets with this method
+
+	#authorize twitter, initialize tweepy
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_key, access_secret)
+	api = tweepy.API(auth)
+
+	#initialize a list to hold all the tweepy Tweets
+	alltweets = []
+
+	#make initial request for most recent tweets (200 is the maximum allowed count)
+	new_tweets = api.user_timeline(screen_name = screen_name,count=200)
+
+	#save most recent tweets
+	alltweets.extend(new_tweets)
+
+	#save the id of the oldest tweet less one
+	oldest = alltweets[-1].id - 1
+
+	#keep grabbing tweets until there are no tweets left to grab
+	while len(new_tweets) > 0:
+		print ("getting tweets gefore %s" % (oldest))
+
+		#all subsiquent requests use the max_id param to prevent duplicates
+		new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
+
+		#save most recent tweets
+		alltweets.extend(new_tweets)
+
+		#update the id of the oldest tweet less one
+		oldest = alltweets[-1].id - 1
+
+		print ("...%s tweets downloaded so far" % (len(alltweets)))
+
+	#transform the tweepy tweets into a 2D array that will populate the csv
+	outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in alltweets]
+
+	#write the csv
+	with open('%s_tweets.csv' % screen_name, 'w') as f:
+		writer = csv.writer(f)
+		writer.writerow(["id","created_at","text"])
+		writer.writerows(outtweets)
+
+	pass
