58e226d8729a33ffd3268d0bbcd14fb5f95e341f
Adam <you@example.com>
2020-07-18 16:23:37 -0400

    started toxicity model

--- models/trainToxicity.py
+++ models/trainToxicity.py
@@ -1,136 +1,59 @@
-from tensorflow.keras.callbacks import ModelCheckpoint
-from tensorflow.keras.layers import Dense, Input
-from tensorflow.keras.optimizers import Adam
+''' This code was run in a kaggle notebook written by me with TPU acceleration. You
+can view the full notebook here: https://www.kaggle.com/khanradcoder/simple-toxicity-classification-with-gru '''
+
+from tensorflow.keras.layers import Activation, Dense, Dropout, Input, Embedding, GRU, GlobalMaxPooling1D, Bidirectional
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.optimizers import RMSprop
 from tensorflow.keras.models import Model
 import tensorflow as tf
-import preprocessing
-import transformers
 import pandas as pd
 import numpy as np
+import json
 
-def fast_encode(texts, tokenizer, chunk_size=256, maxlen=512):
-    """
-    Tokenize text
-    Source: https://www.kaggle.com/xhlulu/jigsaw-tpu-distilbert-with-huggingface-and-keras
-    """
-    tokenizer.enable_truncation(max_length=maxlen)
-    tokenizer.enable_padding(max_length=maxlen)
-    all_ids = []
+# detect and init the TPU
+tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
+tf.config.experimental_connect_to_cluster(tpu)
+tf.tpu.experimental.initialize_tpu_system(tpu)
+# instantiate a distribution strategy
+tpu_strategy = tf.distribute.experimental.TPUStrategy(tpu)
 
-    for i in range(0, len(texts), chunk_size):
-        text_chunk = texts[i:i+chunk_size].tolist()
-        encs = tokenizer.encode_batch(text_chunk)
-        all_ids.extend([enc.ids for enc in encs])
+dataset = pd.read_csv("../input/jigsaw-unintended-bias-in-toxicity-classification/train.csv")
+x = dataset["comment_text"]
+y = dataset["target"]
+max_len = 40
+vocab = 50000
 
-    return np.array(all_ids)
+def tokenizeToxicity(file, vocab_size):
+    tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=vocab_size, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower=True, split=" ", oov_token="<OOV>")
+    tokenizer.fit_on_texts(file)
+    tokenized_file = tokenizer.texts_to_sequences(file)
+    tokenizer_json = tokenizer.to_json()
+    with open('toxictokenizer.json', 'w', encoding='utf-8') as f:
+        f.write(json.dumps(tokenizer_json, ensure_ascii=False))
 
-def build_model(transformer, max_len=512):
-    """
-    Model initalization
-    Source: https://www.kaggle.com/xhlulu/jigsaw-tpu-distilbert-with-huggingface-and-keras
-    """
-    input_word_ids = Input(shape=(max_len,), dtype=tf.int32, name="input_word_ids")
-    sequence_output = transformer(input_word_ids)[0]
-    cls_token = sequence_output[:, 0, :]
-    dense_layer = Dense(224, activation='relu')(cls_token)
-    out = Dense(1, activation='sigmoid')(dense_layer)
+    tokenized_padded_file = np.array(list(tf.keras.preprocessing.sequence.pad_sequences(tokenized_file, max_len, padding='post', truncating='post')))
+    return tokenized_padded_file
 
-    model = Model(inputs=input_word_ids, outputs=out)
-    # model = InceptionV3(input_tensor=input_word_ids, weights='imagenet', include_top=True)
-    model.compile(Adam(lr=1e-5), loss='binary_crossentropy', metrics=['accuracy'])
+x_tokenized = tokenizeToxicity(x, vocab)
+x_train, x_test, y_train, y_test = train_test_split(x_tokenized, y, test_size=0.2, random_state=42)
 
+
+#Build model
+def biGRUnet():
+    inputs = Input(name='inputs',shape=[max_len])
+    layer = Embedding(vocab,max_len,input_length=max_len)(inputs)
+    layer = Bidirectional(GRU(64, return_sequences = True), merge_mode='concat')(layer)
+    layer = GlobalMaxPooling1D()(layer)
+    layer = Dropout(0.5)(layer)
+    layer = Dense(1,name='out_layer')(layer)
+    layer = Activation('sigmoid')(layer)
+    model = Model(inputs=inputs,outputs=layer)
     return model
 
-strategy = tf.distribute.get_strategy()
-AUTO = tf.data.experimental.AUTOTUNE
-EPOCHS = 3
-BATCH_SIZE = 32 * strategy.num_replicas_in_syn
-MAX_LEN = 192
+with tpu_strategy.scope():
+    model = biGRUnet()
+    model.summary()
+    model.compile(loss='binary_crossentropy',optimizer=RMSprop(),metrics=['accuracy'])
 
-DATA_PATH = "data/toxicity"
-
-
-train1 = pd.read_csv(DATA_PATH+"/jigsaw-toxic-comment-train.csv")
-train2 = pd.read_csv(DATA_PATH+"/jigsaw-unintended-bias-train.csv")
-train2.toxic = train2.toxic.round().astype(int)
-
-valid = pd.read_csv(DATA_PATH+'/validation.csv')
-test = pd.read_csv(DATA_PATH+'/test.csv')
-sub = pd.read_csv(DATA_PATH+'/sample_submission.csv')
-
-
-# Combine train1 with a subset of train2
-train = pd.concat([
-    train1[['comment_text', 'toxic']],
-    train2[['comment_text', 'toxic']].query('toxic==1'),
-    train2[['comment_text', 'toxic']].query('toxic==0').sample(n=150000, random_state=3982)
-])
-
-x_train = fast_encode(train.comment_text.astype(str), fast_tokenizer, maxlen=MAX_LEN)
-x_valid = fast_encode(valid.comment_text.astype(str), fast_tokenizer, maxlen=MAX_LEN)
-x_test = fast_encode(test.content.astype(str), fast_tokenizer, maxlen=MAX_LEN)
-
-y_train = train.toxic.values
-y_valid = valid.toxic.values
-
-train_dataset = (
-    tf.data.Dataset
-    .from_tensor_slices((x_train, y_train))
-    .repeat()
-    .shuffle(2048)
-    .batch(BATCH_SIZE)
-    .prefetch(AUTO)
-)
-
-valid_dataset = (
-    tf.data.Dataset
-    .from_tensor_slices((x_valid, y_valid))
-    .batch(BATCH_SIZE)
-    .cache()
-    .prefetch(AUTO)
-)
-
-test_dataset = (
-    tf.data.Dataset
-    .from_tensor_slices(x_test)
-    .batch(BATCH_SIZE)
-)
-
-with strategy.scope():
-    transformer_layer = (
-        transformers.TFDistilBertModel
-        .from_pretrained('distilbert-base-multilingual-cased')
-    )
-    model = build_model(transformer_layer, max_len=MAX_LEN)
-model.summary()
-
-n_steps = x_train.shape[0] // BATCH_SIZE
-train_history = model.fit(
-    train_dataset,
-    steps_per_epoch=n_steps,
-    validation_data=valid_dataset,
-    epochs=EPOCHS
-)
-
-# Plot training & validation accuracy values
-plt.plot(train_history.history['accuracy'])
-plt.plot(train_history.history['val_accuracy'])
-plt.title('Model accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.show()
-
-n_steps = x_valid.shape[0] // BATCH_SIZE
-train_history_2 = model.fit(
-    valid_dataset.repeat(),
-    steps_per_epoch=n_steps,
-    epochs=EPOCHS*2
-)
-
-plt.plot(train_history_2.history['accuracy'])
-plt.title('Model accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train'], loc='upper left')
-plt.show()
+model.fit(x_train,y_train,batch_size=64,epochs=3,validation_data=(x_test, y_test))
+model.save("toxicity.h5")
